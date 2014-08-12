6e4b22c19297cad9f5bc51101dce64d1b543da06
J.J. Idt & Eric Johnson <student@epicodus.com>
2014-08-12 10:22:10 -0700

    Add test for product reads

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -5,4 +5,9 @@ describe 'Product' do
     test_product = Product.create(:name => 'hockey mask', :price => 10.44)
     expect(Product.all.first.price).to eq 10.44
   end
+
+  it 'lets you read the product name' do
+    test_product = Product.create(:name => 'tooth necklace', :price => 99.99)
+    expect(Product.where(:price => 99.99).first.name).to eq 'tooth necklace'
+  end
 end
