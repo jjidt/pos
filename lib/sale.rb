e0e9ecad4bd372ef415387609feca39010caecf3
J.J. Idt & Eric Johnson <student@epicodus.com>
2014-08-12 15:06:40 -0700

    Add calculate total cost on sale

--- lib/sale.rb
+++ lib/sale.rb
@@ -3,5 +3,14 @@ class Sale < ActiveRecord::Base
   has_many :carts
   has_many :products, through: :carts
 
+  def get_total
+    total = 0
+    results = Cart.products_by_sale(self.id)
+    results.each do |x|
+      total += x.price
+    end
+    self.total = total
+    self.save
+  end
 
 end
--- spec/sale_spec.rb
+++ spec/sale_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
-# describe 'Sale' do
-#   it 'updates the total cost for the sale when saved to the database' do
-#     test_sale = Sale.create
-#     binding.pry
-#     test_product = Product.create(:name => 'tooth necklace', :price => 99.99)
-#     test_product2 = Product.create(:name => 'hockey mask', :price => 10.44)
-#     test_product.save
-#     test_product2.save
-#     test_cart = Cart.create({:product_id => test_product.id, })
-
-#     test_sale.create
-#   end
-# end
+describe 'Sale' do
+  it 'updates the total cost for the sale when saved to the database' do
+    sale = Sale.create
+    product1 = Product.create(:name => 'hockey mask', :price => 1.00)
+    product2 = Product.create(:name => 'dirt bike', :price => 1.00)
+    p_id1 = product1.id
+    p_id2 = product2.id
+    cart1 = Cart.create(:product_id => p_id1, :sale_id => sale.id)
+    cart2 = Cart.create(:product_id => p_id2, :sale_id => sale.id)
+    sale.get_total
+    expect(sale.total).to eq 2.00
+  end
+end
