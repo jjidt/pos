00945e1795283961a0e9947cf61073c4fbb9f076
J.J. Idt & Eric Johnson <student@epicodus.com>
2014-08-12 16:52:53 -0700

    Refactor find products by sale id

--- lib/cart.rb
+++ lib/cart.rb
@@ -3,10 +3,13 @@ class Cart < ActiveRecord::Base
   has_many :sales
 
   def self.products_by_sale id
-    results = Cart.where(:sale_id => id)
+    results = Cart.where(:sale_id => id).all
     results = results.map do |result|
       result.product_id
     end
-      Product.where(:id => results)
+    results = results.map do |res|
+      Product.where(:id => res)
+    end
+    results.flatten
   end
 end
